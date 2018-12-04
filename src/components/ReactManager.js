ed6cf8bc0b9bb9fc41b595917c37d4ebe9edb348
kellymorin <kmorin06@gmail.com>
2018-12-04 12:47:16 -0600

    boilerplate code for app in progress

---
+++ src/components/ReactManager.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import NavBar from './nav/NavBar'
+import ApplicationViews from './ApplicationViews'
+import APIManager from '../modules/APIManager'
+
+export default class ReactManager extends Component{
+
+  render(){
+    return(
+      <React.Fragment>
+        <NavBar />
+        <ApplicationViews />
+      </React.Fragment>
+    )
+  }
+}
---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+
+export default class NavBar extends Component{
+render(){
+  return(
+    <React.Fragment>
+      <nav>
+
+      </nav>
+    </React.Fragment>
+  )
+}
+}
