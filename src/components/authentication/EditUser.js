04618f2a204ee5d08a9c120f254d647d1d8535df
kellymorin <kmorin06@gmail.com>
2018-12-06 10:04:23 -0600

    Clean up of commented code.

--- src/components/authentication/EditUser.js
+++ src/components/authentication/EditUser.js
@@ -1,16 +1,4 @@
 /*
-Given the user has an account with Simplified Supper
-When the user is logged in to their account
-Then they should have an option to view their profile information
-
-When the user selects the option to view their profile information
-Then they should be taken to a screen that displays their name, username, email, picture, and password
-And be provided the opportunity to edit their information
-
-When the user selects the affordance to edit their profile information
-Then the information should populate into a form
-And allow the user to update the information
-
 When the user submits updated information
 Then the database should be checked for any conflicts for username or email address
 If there is a conflict
@@ -18,7 +6,7 @@ Then the user should see an error message that prompts them to update the inform
 If there is not a conflict the form should close and the database should be updated
 */
 import React, {Component} from 'react'
-import {Button, Placeholder, Grid, Header, Form} from 'semantic-ui-react'
+import {Placeholder, Grid, Header, Form} from 'semantic-ui-react'
 import {Link} from 'react-router-dom'
 import APIManager from '../../modules/APIManager';
 
