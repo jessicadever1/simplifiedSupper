5007462a1d82cfbedf8f439fa0265d019b5e5c57
kellymorin <kmorin06@gmail.com>
2019-02-19 08:36:39 -0600

    completed refactor of recipe modal

--- src/components/RecipeComponents/RecipeModal/RecipeModal.js
+++ src/components/RecipeComponents/RecipeModal/RecipeModal.js
@@ -7,43 +7,43 @@ export default class RecipeModal extends Component{
 
   state={
     open: false,
-    addToCalendar: false,
-    confirmDelete: false,
+    add_to_calendar: false,
+    confirm_delete: false,
     date: "",
   }
 
-  handleButtonClick=()=>{
-    this.setState({addToCalendar: true})
+  handle_button_click=()=>{
+    this.setState({add_to_calendar: true})
   }
 
-  handleCalendarChange=(name, value)=>{
+  handle_calendar_change=(name, value)=>{
     this.setState({ [name]: value})
   }
 
-  confirmDelete=()=>{
-    this.setState({confirmDelete: true})
+  confirm_delete=()=>{
+    this.setState({confirm_delete: true})
   }
 
-  cancelDelete=()=>{
-    this.setState({confirmDelete: false})
+  cancel_delete=()=>{
+    this.setState({confirm_delete: false})
   }
 
-  makeButtons=(getStarted)=>{
-    if(getStarted === true){
+  make_buttons=(get_started)=>{
+    if(get_started === true){
      return <React.Fragment>
         <Input
           type="date"
-          className={`${this.state.addToCalendar === false ? "isHidden" : ""}`}
-          onChange={(evt)=> this.handleCalendarChange("date", moment(evt.target.value))}
+          className={`${this.state.add_to_calendar === false ? "isHidden" : ""}`}
+          onChange={(evt)=> this.handle_calendar_change("date", moment(evt.target.value))}
           required/>
         <Button
           color="teal"
-          className={`${this.state.addToCalendar === true ? "isHidden" : ""}`}
-          onClick={()=>this.handleButtonClick()} content="Add to Calendar"/>
+          className={`${this.state.add_to_calendar === true ? "isHidden" : ""}`}
+          onClick={()=>this.handle_button_click()} content="Add to Calendar"/>
         <Button
           color="teal"
-          className={`${this.state.addToCalendar === false ? "isHidden" : ""}`}
-          onClick={()=> this.props.handleCalendarChange("newRecipe", this.props.recipeDetails.id, this.state.date)}
+          className={`${this.state.add_to_calendar === false ? "isHidden" : ""}`}
+          onClick={()=> this.props.handle_calendar_change("newRecipe", this.props.recipe_details.id, this.state.date)}
           content="Save Recipe"/>
       </React.Fragment>
     } else{
@@ -51,31 +51,31 @@ export default class RecipeModal extends Component{
         <Input
           type="date"
           defaultValue={moment(this.props.date).format("YYYY-MM-DD")}
-          onChange={(evt)=> this.handleCalendarChange("date", moment(evt.target.value))}/>
+          onChange={(evt)=> this.handle_calendar_change("date", moment(evt.target.value))}/>
         <Button
           color="teal"
-          onClick={()=>this.props.handleCalendarChange( "existingRecipe",this.props.recipeDetails.id, this.state.date)}
+          onClick={()=>this.props.handle_calendar_change( "existingRecipe",this.props.recipe_details.id, this.state.date)}
           content="Save Changes"/>
         <Button
           color="teal"
-          onClick={()=>this.confirmDelete()} content="Delete Recipe"/>
+          onClick={()=>this.confirm_delete()} content="Delete Recipe"/>
       </React.Fragment>
     }
   }
 
-  makeModalStats=(recipe)=>{
+  make_modal_stats=(recipe)=>{
     if(recipe.id === "leftovers" || recipe.id === "eatOut"){
       return
     } else{
       return <Statistic.Group size='mini' widths="three">
-    <Statistic value={this.props.recipeDetails.ingredientLines.length} label="Ingredients"/>
-    <Statistic value={this.props.recipeDetails.nutritionEstimates[0].value} label="Calories"/>
-    <Statistic value={this.props.recipeDetails.totalTime} label="Total Time"/>
+    <Statistic value={this.props.recipe_details.ingredientLines.length} label="Ingredients"/>
+    <Statistic value={this.props.recipe_details.nutritionEstimates[0].value} label="Calories"/>
+    <Statistic value={this.props.recipe_details.totalTime} label="Total Time"/>
   </Statistic.Group>
     }
   }
 
-  makeIngredientsList=(recipe)=>{
+  make_ingredients_list=(recipe)=>{
     if(recipe.id === "leftovers" || recipe.id === "eatOut"){
       return
     } else{
@@ -90,38 +90,38 @@ export default class RecipeModal extends Component{
     }
   }
 
-  makeInstructionButton=(recipe)=>{
+  make_instruction_button=(recipe)=>{
     if(recipe.id === "leftovers" || recipe.id === "eatOut"){
       return
     } else{
-      return <a href={this.props.recipeDetails.source.sourceRecipeUrl} target="_blank" rel="noopener noreferrer"><Button content="Read Instructions"/></a>
+      return <a href={this.props.recipe_details.source.sourceRecipeUrl} target="_blank" rel="noopener noreferrer"><Button content="Read Instructions"/></a>
     }
   }
 
   render(){
-    if(this.state.confirmDelete === true){
-      return <Confirm open={this.state.confirmDelete} onCancel={this.cancelDelete} onConfirm={()=>this.props.deleteRecipe()}/>
+    if(this.state.confirm_delete === true){
+      return <Confirm open={this.state.confirm_delete} onCancel={this.cancel_delete} onConfirm={()=>this.props.delete_recipe()}/>
     }
     return(
       <React.Fragment>
-        <Modal open={this.props.open} onClose={this.props.closeRecipeDetails}>
-          <Modal.Header content={this.props.recipeDetails.name}/>
+        <Modal open={this.props.open} onClose={this.props.close_recipe_details}>
+          <Modal.Header content={this.props.recipe_details.name}/>
           <Modal.Content image>
-            <Image wrapped size='medium' src={this.props.recipeDetails.images[0].hostedLargeUrl} />
+            <Image wrapped size='medium' src={this.props.recipe_details.images[0].hostedLargeUrl} />
             <Modal.Description>
                 <div>
-                  <a href={this.props.recipeDetails.source.sourceRecipeUrl} target="_blank" rel="noopener noreferrer">
-                  {this.props.recipeDetails.source.sourceDisplayName}
+                  <a href={this.props.recipe_details.source.sourceRecipeUrl} target="_blank" rel="noopener noreferrer">
+                  {this.props.recipe_details.source.sourceDisplayName}
                   </a>
                 </div>
                 {
-                  this.makeModalStats(this.props.recipeDetails)
+                  this.makeModalStats(this.props.recipe_details)
                 }
                 {
-                  this.makeIngredientsList(this.props.recipeDetails)
+                  this.makeIngredientsList(this.props.recipe_details)
                 }
                 {
-                  this.makeInstructionButton(this.props.recipeDetails)
+                  this.makeInstructionButton(this.props.recipe_details)
                 }
               {/* <div>
                 <a href={this.props.recipeDetails.attribution.link} >{this.props.recipeDetails.attribution.text}</a>
@@ -130,7 +130,7 @@ export default class RecipeModal extends Component{
             </Modal.Description>
           </Modal.Content>
           <Modal.Actions>
-            {this.makeButtons(this.props.getStarted)}
+            {this.makeButtons(this.props.get_started)}
           </Modal.Actions>
         </Modal>
       </React.Fragment>
